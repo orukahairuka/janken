95227a22ca1bee9a6d8ef7b70c2847f03df65549
irukahaoruka <erika03301109@gmail.com>
2025-06-30 22:15:59 +0900

    step2-button-basics

--- janken/step2-button-basics/ContentView.swift
+++ janken/step2-button-basics/ContentView.swift
@@ -1,11 +1,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
         VStack {
             Text("じゃんけんアプリ")
                 .font(.largeTitle)
                 .foregroundColor(.blue)
+            
+            Text("タップ回数: \(tapCount)")
+                .font(.title2)
+                .padding()
+            
+            Button("タップしてね") {
+                tapCount += 1
+            }
+            .font(.title)
+            .padding()
         }
     }
 }
